29e0d5546b1a556cb624261f71a3cdae97cdb2a2
HiDeo <hideo.sup@gmail.com>
2018-07-22 15:51:38 +0200

    Increase logs limits

--- src/constants/logs.ts
+++ src/constants/logs.ts
@@ -2,10 +2,10 @@ export default {
   /**
    * Number of logs to store for a channel.
    */
-  Max: 800,
+  Max: 1000,
 
   /**
    * Threshold for the number of logs to store to avoid too many updates.
    */
-  Threshold: 200,
+  Threshold: 300,
 }
