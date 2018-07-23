ffe7501b5fd10321dbeecfbb3249f3f0456e2561
HiDeo <hideo.sup@gmail.com>
2018-07-23 16:32:41 +0200

    Update bug report button template

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -216,7 +216,28 @@ Thanks for filing an issue 😄 !
 Please provide as much details as possible, including screenshots if necessary.
 -->
 
-#### Environment
+**Describe the bug**
+
+A clear and concise description of what the bug is.
+
+**To Reproduce**
+
+Steps to reproduce the behavior:
+
+1. Go to '...'
+2. Click on '....'
+3. Scroll down to '....'
+4. See error
+
+**Expected behavior**
+
+A clear and concise description of what you expected to happen.
+
+**Screenshots**
+
+If applicable, add screenshots to help explain your problem.
+
+**Environment**
 
 | Software         | Version
 | ---------------- | -------
