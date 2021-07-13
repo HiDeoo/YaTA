8b44a7a80988099a6547a9521633f5e999e3a909
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2021-07-13 18:32:31 +0200

    Update Blueprint

--- src/components/ExternalButton.tsx
+++ src/components/ExternalButton.tsx
@@ -1,11 +1,11 @@
-import { AnchorButton, IButtonProps } from '@blueprintjs/core'
+import { AnchorButton, ButtonProps } from '@blueprintjs/core'
 import * as React from 'react'
 
 /**
  * ExternalButton Component.
  */
-const ExternalButton: React.FunctionComponent<IButtonProps & React.AnchorHTMLAttributes<HTMLAnchorElement>> = (
-  props
-) => <AnchorButton target="_blank" {...props} />
+const ExternalButton: React.FunctionComponent<
+  ButtonProps<HTMLAnchorElement> & React.AnchorHTMLAttributes<HTMLAnchorElement>
+> = (props) => <AnchorButton target="_blank" {...props} />
 
 export default ExternalButton
